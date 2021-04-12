86d37d6fe481c7b8d57aede92789b9e883ca3ce2
Demis Rizzotto <demis.rizzotto@gaming1.com>
2021-04-13 00:48:51 +0200

    respond with original headers

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-// addEventListener('fetch', event => {
-//   event.respondWith(handleRequest(event.request))
-// })
-// /**
-//  * Respond with hello worker text
-//  * @param {Request} request
-//  */
-// async function handleRequest(request) {
-//   return new Response('Hello worker!', {
-//     headers: { 'content-type': 'text/plain' },
-//   })
-// }
-
-addEventListener("fetch", event => {
-  return event.respondWith(
-    fetch("https://httpbin.org/ip")
-  )
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
 })
+
+/**
+ * Respond to the request
+ * @param {Request} request
+ */
+async function handleRequest(e) {
+
+  let empty = '';
+  for (const element of e.headers) {
+    empty = empty.concat(element[0], " => ", element[1], "\n")
+  }
+
+
+  var host = e.headers.get("host")
+  return new Response(a, {
+    status: 200
+  })
+}
+
+// // Proxy request to another URL
+// addEventListener("fetch", event => {
+//   return event.respondWith(
+//     fetch("https://httpbin.org/ip")
+//   )
+// })
