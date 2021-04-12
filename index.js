13c11795ec559f8d3704eef4f4e1122ef9e47abc
Demis Rizzotto <demis.rizzotto@gaming1.com>
2021-04-13 01:11:44 +0200

    json response headers

--- index.js
+++ index.js
@@ -6,16 +6,26 @@ addEventListener('fetch', event => {
  * Respond to the request
  * @param {Request} request
  */
-async function handleRequest(e) {
+async function handleRequest(request) {
 
-  let empty = '';
-  for (const element of e.headers) {
-    empty = empty.concat(element[0], " => ", element[1], "\n")
+  // const clientIP = request.headers.get("CF-Connecting-IP")
+
+  // let response = '';
+  // for (const element of request.headers) {
+  //   response = response.concat(element[0], " => ", element[1], "\n")
+  // }
+
+  const data = {
+    source_ip: request.headers.get("CF-Connecting-IP"),
+    country: request.headers.get("cf-ipcountry")
   }
 
-  var host = e.headers.get("host")
-  return new Response(empty, {
-    status: 200
+  const json = JSON.stringify(data, null, 2)
+
+  return new Response(json, {
+    headers: {
+      "content-type": "application/json;charset=UTF-8"
+    }
   })
 }
 
